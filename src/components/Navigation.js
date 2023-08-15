e83f1b82dd0170b5c789457f031844ee3243f383
Nils <nilsotto@gmx.de>
2023-08-15 17:04:03 +0200

    add navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import { NavLink } from "react-router-dom";
+import { StyledNavigation } from "./styles";
+
+export default function Navigation() {
+  return (
+    <StyledNavigation>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/Cart">Warenkorb</NavLink>
+    </StyledNavigation>
+  );
+}
---
+++ src/components/styles.js
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const StyledAppGrid = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 50px 1fr;
+`;
+
+export const StyledNavigation = styled.nav`
+  justify-self: center;
+  display: flex;
+  gap: 10px;
+`;
